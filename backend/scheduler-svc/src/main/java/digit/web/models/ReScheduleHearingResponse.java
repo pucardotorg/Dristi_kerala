4bc2ae298c194594fdf27c4df876a1f75245a19f
prasanna2400 <prasannajeet.v@beehyv.com>
2024-04-23 22:25:43 +0530

    added persister and workflow service

---
+++ backend/scheduler-svc/src/main/java/digit/web/models/ReScheduleHearingResponse.java
@@ -0,0 +1,28 @@
+package digit.web.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.egov.common.contract.response.ResponseInfo;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ReScheduleHearingResponse {
+
+
+    @JsonProperty("ResponseInfo")
+    @Valid
+    private ResponseInfo responseInfo = null;
+
+    @JsonProperty("Hearing")
+    @Valid
+    private List<ReScheduleHearing> reScheduleHearings = null;
+}
