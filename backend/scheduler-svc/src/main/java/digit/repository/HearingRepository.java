a3b7325f23ce1421bf95d3dc12df6c084b7d22b3
prasanna2400 <prasannajeet.v@beehyv.com>
2024-04-24 22:15:15 +0530

    created calendar repository ,hearing repository and hearing query builder

---
+++ backend/scheduler-svc/src/main/java/digit/repository/HearingRepository.java
@@ -0,0 +1,50 @@
+package digit.repository;
+
+
+import digit.repository.querybuilder.CalendarQueryBuilder;
+import digit.repository.querybuilder.HearingQueryBuilder;
+import digit.repository.rowmapper.CalendarRowMapper;
+import digit.repository.rowmapper.HearingRowMapper;
+import digit.web.models.HearingSearchCriteria;
+import digit.web.models.JudgeCalendar;
+import digit.web.models.JudgeSearchCriteria;
+import digit.web.models.ScheduleHearing;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Repository
+public class HearingRepository {
+    @Autowired
+    private HearingQueryBuilder queryBuilder;
+
+    @Autowired
+    private HearingRowMapper rowMapper;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<ScheduleHearing> getJudgeHearing(HearingSearchCriteria hearingSearchCriteria) {
+
+        List<String> preparedStmtList = new ArrayList<>();
+        String query = queryBuilder.getJudgeHearingQuery(hearingSearchCriteria, preparedStmtList);
+        log.debug("Final query: " + query);
+        return jdbcTemplate.query(query, rowMapper);
+
+    }
+
+    public List<String> getAvailableDatesOfJudges(HearingSearchCriteria hearingSearchCriteria) {
+
+        List<String> preparedStmtList = new ArrayList<>();
+        String query = queryBuilder.getJudgeAvailableDatesQuery(hearingSearchCriteria, preparedStmtList);
+        log.debug("Final query: " + query);
+        return jdbcTemplate.queryForList(query, String.class);
+
+
+    }
+}
---
+++ backend/scheduler-svc/src/main/java/digit/repository/querybuilder/HearingQueryBuilder.java
@@ -0,0 +1,58 @@
+package digit.repository.querybuilder;
+
+import digit.web.models.HearingSearchCriteria;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Slf4j
+public class HearingQueryBuilder {
+
+    private final String BASE_APPLICATION_QUERY = "SELECT  hb.hearingbookingid, hb.tenantid, hb.courtid, hb.judgeid, hb.caseid, hb.date, hb.eventtype, hb.title, hb.description, hb.status, hb.starttime, hb.endtime, hb.createdby,hb.lastmodifiedby,hb.createdtime,hb.lastmodifiedtime, hb.rowversion ";
+
+    private static final String FROM_TABLES = " FROM hearing_booking hb ";
+
+    private final String ORDER_BY = " ORDER BY ";
+
+    private final String GROUP_BY = " GROUP BY ";
+
+    private final String LIMIT_OFFSET = " LIMIT ? OFFSET ?";
+
+    public String getJudgeHearingQuery(HearingSearchCriteria hearingSearchCriteria, List<String> preparedStmtList) {
+
+        StringBuilder query = new StringBuilder(BASE_APPLICATION_QUERY);
+        query.append(FROM_TABLES);
+
+        if (!ObjectUtils.isEmpty(hearingSearchCriteria.getJudgeId())) {
+            addClauseIfRequired(query, preparedStmtList);
+            query.append(" hb.judgeId = ? ");
+            preparedStmtList.add(hearingSearchCriteria.getJudgeId());
+
+        }
+
+        return query.toString();
+    }
+
+    public String getJudgeAvailableDatesQuery(HearingSearchCriteria hearingSearchCriteria, List<String> preparedStmtList) {
+        StringBuilder query = new StringBuilder("SELECT hb.date ");
+        query.append("FROM (");
+        query.append("SELECT hb.date, SUM(TIMESTAMPDIFF(MINUTE, hb.starttime, hb.endtime)) / 60 AS total_hours");
+        query.append(FROM_TABLES).append(GROUP_BY).append(" hb.date ");
+        query.append(") AS meeting_hours ");
+
+        return query.toString();
+    }
+
+    private void addClauseIfRequired(StringBuilder query, List<String> preparedStmtList) {
+        if (preparedStmtList.isEmpty()) {
+            query.append(" WHERE ");
+        } else {
+            query.append(" AND ");
+        }
+    }
+
+
+}
