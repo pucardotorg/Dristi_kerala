22e3ea0dda2f8897ad3f428f349c6b1869cea1f8
prasanna2400 <prasannajeet.v@beehyv.com>
2024-05-06 10:34:29 +0530

    created opt out apis and update data flow in service

---
+++ backend/scheduler-svc/src/main/resources/db/migration/main/V20240505144400__oo_opt_out_ddl.sql
@@ -0,0 +1,19 @@
+CREATE TABLE opt_out
+(
+
+    id                              character varying(64),
+    individualId                    character varying(64),
+    judgeId                         character varying(64),
+    caseId                          character varying(64),
+    hearingRescheduleRequestId      character varying(64),
+    optoutDates                     jsonb
+    createdBy                       character varying(64),
+    createdTime                     bigint,
+    lastModifiedBy                  character varying(64),
+    lastModifiedTime                bigint,
+    rowVersion                      bigint,
+    tenantId                        character varying(1000),
+
+    CONSTRAINT pk_hearing_booking_id PRIMARY KEY (hearingBookingId)
+
+);
