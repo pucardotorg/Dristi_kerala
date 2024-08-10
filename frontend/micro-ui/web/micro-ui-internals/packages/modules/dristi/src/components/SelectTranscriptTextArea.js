f884cb9fd9d9de192007e202af86d78ef29e6894
Teja <divyateja.konatam@beehyv.com>
2024-08-10 19:14:19 +0530

    changed the styling for transcription

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/SelectTranscriptTextArea.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/SelectTranscriptTextArea.js
@@ -338,102 +338,92 @@ function SelectTranscriptTextArea({ t, config, formData = {}, onSelect, errors }
           </span>
         )}
       </div>
-      <textarea
-        value={isRecording ? editableTranscription : formdata?.[config.key]?.[input.name]}
-        onChange={(data) => {
-          handleChange(data, input);
-        }}
-        rows={5}
-        maxLength={400}
-        className={`custom-textarea-style${errors[config.key] ? " alert-error-border" : ""}`}
-        placeholder={t(input?.placeholder)}
-        disabled={config.disable}
-      ></textarea>
-      {/* <TextArea
-        value={isRecording ? editableTranscription : formdata?.[config.key]?.[input.name]}
-        onChange={(data) => {
-          handleChange(data, input);
-        }}
-        rows={5}
-        maxLength={400}
-        style={{ width: "100%", minHeight: "40vh" }}
-        className={`custom-textarea-style${errors[config.key] ? " alert-error-border" : ""}`}
-        placeholder={t(input?.placeholder)}
-        disabled={config.disable}
-      /> */}
-      <div>
-        <input type="radio" id="micInput" name="inputSource" value="mic" defaultChecked ref={inputSourceRef} style={{ display: "none" }} />
+      <div style={{ position: "relative" }}>
+        <textarea
+          value={isRecording ? editableTranscription : formdata?.[config.key]?.[input.name]}
+          onChange={(data) => {
+            handleChange(data, input);
+          }}
+          rows={5}
+          maxLength={400}
+          className={`custom-textarea-style${errors[config.key] ? " alert-error-border" : ""}`}
+          placeholder={t(input?.placeholder)}
+          disabled={config.disable}
+        ></textarea>
+        <div style={{ position: "absolute", bottom: "7px", right: "7px", textAlign: "right", width: "100%" }}>
+          <input type="radio" id="micInput" name="inputSource" value="mic" defaultChecked ref={inputSourceRef} style={{ display: "none" }} />
 
-        {!isConnected && (
-          <div style={{ textAlign: "right" }}>
-            <button
-              onClick={() => {
-                createRoom();
-                setIsConnected(true);
-              }}
-              title="Connect"
-            >
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_4370_85284)">
-                  <path
-                    d="M12 2C10.34 2 9 3.34 9 5V7H15V5C15 3.34 13.66 2 12 2ZM5 8C3.34 8 2 9.34 2 11V13C2 14.66 3.34 16 5 16H7V8H5ZM19 8H17V16H19C20.66 16 22 14.66 22 13V11C22 9.34 20.66 8 19 8ZM11 18H13V20H11V18Z"
-                    fill="#3D3C3C"
-                  />
-                  <path d="M7 8L17 8" stroke="#3D3C3C" stroke-width="2" stroke-linecap="round" />
-                  <path d="M12 16L12 20" stroke="#3D3C3C" stroke-width="2" stroke-linecap="round" />
-                </g>
-                <defs>
-                  <clipPath id="clip0_4370_85284">
-                    <rect width="24" height="24" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </button>
-          </div>
-        )}
-        {isConnected && webSocketStatus === "Connected" && !isRecording && (
-          <div style={{ textAlign: "right" }}>
-            <button onClick={startRecording} title="Start Recording">
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_4370_85283)">
-                  <path
-                    d="M12 13C13.66 13 14.99 11.66 14.99 10L15 4C15 2.34 13.66 1 12 1C10.34 1 9 2.34 9 4V10C9 11.66 10.34 13 12 13ZM19 10H17.3C17.3 13 14.76 15.1 12 15.1C9.24 15.1 6.7 13 6.7 10H5C5 13.41 7.72 16.23 11 16.72V20H13V16.72C16.28 16.23 19 13.41 19 10Z"
-                    fill="#3D3C3C"
-                  />
-                  <path d="M19 5L5 19" stroke="#3D3C3C" stroke-width="2" stroke-linecap="round" />
-                  <path
-                    d="M7 24H9V22H7V24ZM11 24H13V22H11V24ZM15 24H17V22H15V24ZM12 20V16.72C8.72 16.23 6 13.41 6 10H7.7C7.7 13 10.24 15.1 12 15.1C13.76 15.1 16.3 13 16.3 10H18C18 13.41 15.28 16.23 12 16.72V20H12Z"
-                    fill="#3D3C3C"
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0_4370_85283">
-                    <rect width="24" height="24" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </button>
-          </div>
-        )}
-        {isConnected && isRecording && (
-          <div style={{ textAlign: "right" }}>
-            <button onClick={stopRecording} title="Stop Recording">
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_4370_85283)">
-                  <path
-                    d="M7 24H9V22H7V24ZM12 13C13.66 13 14.99 11.66 14.99 10L15 4C15 2.34 13.66 1 12 1C10.34 1 9 2.34 9 4V10C9 11.66 10.34 13 12 13ZM11 24H13V22H11V24ZM15 24H17V22H15V24ZM19 10H17.3C17.3 13 14.76 15.1 12 15.1C9.24 15.1 6.7 13 6.7 10H5C5 13.41 7.72 16.23 11 16.72V20H13V16.72C16.28 16.23 19 13.41 19 10Z"
-                    fill="#3D3C3C"
-                  />
-                </g>
-                <defs>
-                  <clipPath id="clip0_4370_85283">
-                    <rect width="24" height="24" fill="white" />
-                  </clipPath>
-                </defs>
-              </svg>
-            </button>
-          </div>
-        )}
+          {!isConnected && (
+            <div style={{ textAlign: "right" }}>
+              <button
+                onClick={() => {
+                  createRoom();
+                  setIsConnected(true);
+                }}
+                title="Connect"
+              >
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_4370_85284)">
+                    <path
+                      d="M12 2C10.34 2 9 3.34 9 5V7H15V5C15 3.34 13.66 2 12 2ZM5 8C3.34 8 2 9.34 2 11V13C2 14.66 3.34 16 5 16H7V8H5ZM19 8H17V16H19C20.66 16 22 14.66 22 13V11C22 9.34 20.66 8 19 8ZM11 18H13V20H11V18Z"
+                      fill="#3D3C3C"
+                    />
+                    <path d="M7 8L17 8" stroke="#3D3C3C" stroke-width="2" stroke-linecap="round" />
+                    <path d="M12 16L12 20" stroke="#3D3C3C" stroke-width="2" stroke-linecap="round" />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_4370_85284">
+                      <rect width="24" height="24" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </button>
+            </div>
+          )}
+          {isConnected && webSocketStatus === "Connected" && !isRecording && (
+            <div style={{ textAlign: "right" }}>
+              <button onClick={startRecording} title="Start Recording">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_4370_85283)">
+                    <path
+                      d="M12 13C13.66 13 14.99 11.66 14.99 10L15 4C15 2.34 13.66 1 12 1C10.34 1 9 2.34 9 4V10C9 11.66 10.34 13 12 13ZM19 10H17.3C17.3 13 14.76 15.1 12 15.1C9.24 15.1 6.7 13 6.7 10H5C5 13.41 7.72 16.23 11 16.72V20H13V16.72C16.28 16.23 19 13.41 19 10Z"
+                      fill="#3D3C3C"
+                    />
+                    <path d="M19 5L5 19" stroke="#3D3C3C" stroke-width="2" stroke-linecap="round" />
+                    <path
+                      d="M7 24H9V22H7V24ZM11 24H13V22H11V24ZM15 24H17V22H15V24ZM12 20V16.72C8.72 16.23 6 13.41 6 10H7.7C7.7 13 10.24 15.1 12 15.1C13.76 15.1 16.3 13 16.3 10H18C18 13.41 15.28 16.23 12 16.72V20H12Z"
+                      fill="#3D3C3C"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_4370_85283">
+                      <rect width="24" height="24" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </button>
+            </div>
+          )}
+          {isConnected && isRecording && (
+            <div style={{ textAlign: "right" }}>
+              <button onClick={stopRecording} title="Stop Recording">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <g clip-path="url(#clip0_4370_85283)">
+                    <path
+                      d="M7 24H9V22H7V24ZM12 13C13.66 13 14.99 11.66 14.99 10L15 4C15 2.34 13.66 1 12 1C10.34 1 9 2.34 9 4V10C9 11.66 10.34 13 12 13ZM11 24H13V22H11V24ZM15 24H17V22H15V24ZM19 10H17.3C17.3 13 14.76 15.1 12 15.1C9.24 15.1 6.7 13 6.7 10H5C5 13.41 7.72 16.23 11 16.72V20H13V16.72C16.28 16.23 19 13.41 19 10Z"
+                      fill="#3D3C3C"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_4370_85283">
+                      <rect width="24" height="24" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              </button>
+            </div>
+          )}
+        </div>
       </div>
       {/* </TextArea> */}
       {errors[config.key] && <CardLabelError>{t(errors[config.key].msg || "CORE_REQUIRED_FIELD_ERROR")}</CardLabelError>}
