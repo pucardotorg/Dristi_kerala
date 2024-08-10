1b7f4192d79a2dbeb26124c9217d5e1c86a166cf
prasanna2400 <prasannajeet.v@beehyv.com>
2024-08-10 19:14:28 +0530

    edits for email body

--- backend/summons-svc/src/main/java/digit/service/EmailService.java
+++ backend/summons-svc/src/main/java/digit/service/EmailService.java
@@ -4,12 +4,15 @@ import digit.config.Configuration;
 import digit.kafka.Producer;
 import digit.web.models.Email;
 import digit.web.models.EmailRequest;
+import digit.web.models.Task;
 import digit.web.models.TaskRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.egov.common.contract.models.Document;
 import org.egov.common.contract.request.RequestInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Set;
 
 import static digit.config.ServiceConstants.TASK_EMAIL_TEMPLATE;
@@ -35,17 +38,27 @@ public class EmailService {
     private EmailRequest getEmailRequestFromTask(TaskRequest request) {
         EmailRequest emailRequest = new EmailRequest();
         RequestInfo requestInfo = request.getRequestInfo();
-        Email email = new Email();
-
-        email.setHTML(true);
-        email.setBody("{\"taskNumber\":" + request.getTask().getTaskNumber() + "}");
-        email.setTemplateCode(TASK_EMAIL_TEMPLATE);
-        email.setEmailTo(Set.of(request.getTask().getTaskDetails().getRespondentDetails().getEmail()));
-        email.setSubject("Task Assigned");
-
+        Email email = getEmail(request.getTask());
         emailRequest.setRequestInfo(requestInfo);
         emailRequest.setEmail(email);
-
         return emailRequest;
+    }
+
+    private Email getEmail(Task task) {
+        Email email = new Email();
+        email.setTenantId(task.getTenantId());
+        email.setHTML(true);
+        email.setBody("{\"taskNumber\":" + task.getTaskNumber() + "}");
+        email.setTemplateCode(TASK_EMAIL_TEMPLATE);
+        email.setEmailTo(Set.of(task.getTaskDetails().getRespondentDetails().getEmail()));
+        email.setSubject("Task Assigned");
+        if(task.getDocuments() != null) {
+            HashMap<String, String> fileStoreIds = new HashMap<>();
+            for(Document document : task.getDocuments()) {
+                fileStoreIds.put(document.getFileStore(), document.getFileStore());
+            }
+            email.setFileStoreId(fileStoreIds);
+        }
+        return email;
     }
 }
