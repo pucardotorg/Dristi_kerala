f45695e09dd244eb207859cf8fb2e6c8b82db00a
utpalBeehyv <utpal.kant@beehyv.com>
2024-08-23 17:43:55 +0530

    efilling_hardcoded_fix (#759)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/SelectSignature.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/dristi/src/components/SelectSignature.js
@@ -2,7 +2,6 @@ import React, { useEffect, useMemo } from "react";
 import SignatureCard from "./SignatureCard";
 import { DRISTIService } from "../services";
 import isEqual from "lodash/isEqual";
-import { getFilestoreId } from "../Utils/fileStoreUtil";
 
 function SelectSignature({ t, config, onSelect, formData = {}, errors }) {
   const inputs = useMemo(
@@ -53,7 +52,7 @@ function SelectSignature({ t, config, onSelect, formData = {}, errors }) {
       localStorage.removeItem("formdata");
     }
   }, [isSignSuccess, formData]);
-  const filestoreId = getFilestoreId();
+
   const EsignFileStoreID = localStorage.getItem("fileStoreId");
   const handleAadharClick = async (data, name) => {
     try {
@@ -64,7 +63,7 @@ function SelectSignature({ t, config, onSelect, formData = {}, errors }) {
           uidToken: "3456565",
           consent: "6564",
           authType: "6546",
-          fileStoreId: EsignFileStoreID ? EsignFileStoreID : "4824356d-e582-48bc-8929-c53a1bdb2bf1",
+          fileStoreId: EsignFileStoreID ? EsignFileStoreID : "bfd9ad76-4ed2-4549-b6ee-073b9b829e90",
           tenantId: "kl",
           pageModule: "ci",
         },
