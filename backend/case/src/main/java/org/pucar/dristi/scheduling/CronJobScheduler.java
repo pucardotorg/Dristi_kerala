359066a15ace6e5472fde0d36e7f41e77a181243
harshkumar8789 <harsh.kumar@beehyv.com>
2024-08-26 15:34:15 +0530

    cron-job fixes.

--- backend/case/src/main/java/org/pucar/dristi/scheduling/CronJobScheduler.java
+++ backend/case/src/main/java/org/pucar/dristi/scheduling/CronJobScheduler.java
@@ -133,7 +133,7 @@ public class CronJobScheduler {
                 CaseCriteria criteria = CaseCriteria.builder()
                         .status(Collections.singletonList("PAYMENT_PENDING"))
                         .filingToDate(LocalDate.now().atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli())
-                        .filingFromDate(LocalDate.now().minusDays(Integer.getInteger(config.getUserNotificationPeriod())).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli())
+                        .filingFromDate(LocalDate.now().minusDays(Integer.parseInt(config.getUserNotificationPeriod())).atStartOfDay(ZoneOffset.UTC).toInstant().toEpochMilli())
                         .pagination(pagination)
                         .build();
 
