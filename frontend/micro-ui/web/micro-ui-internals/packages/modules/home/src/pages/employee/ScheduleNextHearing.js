74cf39e49a753d1e6ef7588d5567cd4f08939200
Suraj Majumdar <suraj.majumdar@beehyv.com>
2024-08-27 12:16:28 +0530

    closed pending task of reschedule hearing for judge

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/ScheduleNextHearing.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/ScheduleNextHearing.js
@@ -334,27 +334,12 @@ function ScheduleNextHearing({
       setIsSubmitDisabled(true);
       HomeService.customApiService(Urls.orderCreate, reqBody, { tenantId })
         .then(async (res) => {
-          // await HomeService.customApiService(Urls.pendingTask, {
-          //   pendingTask: {
-          //     name: "Schedule Hearing",
-          //     entityType: "case",
-          //     referenceId: `MANUAL_${caseDetails?.filingNumber}`,
-          //     status: "SCHEDULE_HEARING",
-          //     assignedTo: [],
-          //     assignedRole: ["JUDGE_ROLE"],
-          //     cnrNumber: null,
-          //     filingNumber: caseDetails?.filingNumber,
-          //     isCompleted: true,
-          //     additionalDetails: {},
-          //     tenantId,
-          //   },
-          // });
           await HomeService.customApiService(Urls.pendingTask, {
             pendingTask: {
-              name: "Completed",
+              name: "Create Order for rescheduling the hearing",
               entityType: "order-managelifecycle",
               referenceId: hearingId,
-              status: "DRAFT_IN_PROGRESS",
+              status: "RESCHEDULE_HEARING",
               assignedTo: [],
               assignedRole: [],
               cnrNumber: cnrNumber,
