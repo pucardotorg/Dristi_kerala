788d82ddb392270a336761720b008dd261bf30fc
surajmajumdar0001 <149581379+surajmajumdar0001@users.noreply.github.com>
2024-08-28 12:44:37 +0530

    Fixed the individual id bug (#826)

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/ScheduleHearing.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/home/src/pages/employee/ScheduleHearing.js
@@ -172,15 +172,16 @@ function ScheduleHearing({
   };
 
   const fetchBasicUserInfo = async () => {
+    const userInfo = JSON.parse(window.localStorage.getItem("user-info"));
     const individualData = await window?.Digit.DRISTIService.searchIndividualUser(
       {
         Individual: {
-          userUuid: [caseDetails?.auditDetails?.createdBy],
+          userUuid: [userInfo?.uuid],
         },
       },
       { tenantId, limit: 1000, offset: 0 },
       "",
-      caseDetails?.auditDetails?.createdBy
+      userInfo
     );
     return individualData?.Individual?.[0];
   };
