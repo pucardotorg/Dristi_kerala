449555442312fc7a63bfe350ac46ef437cd52123
Ajith-beehyv <150236657+Ajith-beehyv@users.noreply.github.com>
2024-08-29 14:09:16 +0530

    fix task details syntax in row mapper (#880)

--- backend/task/src/main/java/org/pucar/dristi/repository/rowmapper/TaskRowMapper.java
+++ backend/task/src/main/java/org/pucar/dristi/repository/rowmapper/TaskRowMapper.java
@@ -64,7 +64,7 @@ public class TaskRowMapper implements ResultSetExtractor<List<Task>> {
                             .dateCloseBy(rs.getLong("datecloseby"))
                             .dateClosed(rs.getLong("dateclosed"))
                             .taskDescription(rs.getString("taskdescription"))
-                            .taskDetails(objectMapper.readValue(rs.getString("task_details"), Object.class))
+                            .taskDetails(objectMapper.readValue(rs.getString("taskdetails"), Object.class))
                             .taskType(rs.getString("tasktype"))
                             .status(rs.getString("status"))
                             .assignedTo(getObjectFromJson(rs.getString("assignedto"), new TypeReference<AssignedTo>() {
