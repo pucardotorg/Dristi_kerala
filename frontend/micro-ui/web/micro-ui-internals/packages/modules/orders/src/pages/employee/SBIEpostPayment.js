3d2279513154ca3b54cabb0846b65f371920a9eb
roopesh.sagar <roopesh.sagar@beehyv.com>
2024-08-29 12:19:48 +0530

    summons post payment bugs resolved

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/orders/src/pages/employee/SBIEpostPayment.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Loader } from "@egovernments/digit-ui-components";
+
+function SBIEpostPayment() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <Loader />
+    </div>
+  );
+}
+
+export default SBIEpostPayment;
