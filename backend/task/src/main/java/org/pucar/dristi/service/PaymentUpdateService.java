3abffd4965e1a0a93b18807161414c19072e38a0
harshkumar8789 <harsh.kumar@beehyv.com>
2024-09-04 14:26:19 +0530

    update summon epost payment.

--- backend/task/src/main/java/org/pucar/dristi/service/PaymentUpdateService.java
+++ backend/task/src/main/java/org/pucar/dristi/service/PaymentUpdateService.java
@@ -3,12 +3,14 @@ package org.pucar.dristi.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.egov.common.contract.models.RequestInfoWrapper;
 import org.egov.common.contract.models.Workflow;
 import org.egov.common.contract.request.RequestInfo;
 import org.egov.common.contract.request.Role;
 import org.egov.tracer.model.CustomException;
 import org.pucar.dristi.config.Configuration;
 import org.pucar.dristi.kafka.Producer;
+import org.pucar.dristi.repository.ServiceRequestRepository;
 import org.pucar.dristi.repository.TaskRepository;
 import org.pucar.dristi.util.WorkflowUtil;
 import org.pucar.dristi.web.models.*;
@@ -16,6 +18,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
@@ -35,13 +40,16 @@ public class PaymentUpdateService {
 
     private Configuration config;
 
+    private ServiceRequestRepository serviceRequestRepository;
+
     @Autowired
-    public PaymentUpdateService(WorkflowUtil workflowUtil, ObjectMapper mapper, TaskRepository repository, Producer producer, Configuration config) {
+    public PaymentUpdateService(WorkflowUtil workflowUtil, ObjectMapper mapper, TaskRepository repository, Producer producer, Configuration config, ServiceRequestRepository serviceRequestRepository) {
         this.workflowUtil = workflowUtil;
         this.mapper = mapper;
         this.repository = repository;
         this.producer = producer;
         this.config = config;
+        this.serviceRequestRepository = serviceRequestRepository;
     }
 
     public void process(Map<String, Object> record) {
@@ -69,39 +77,83 @@ public class PaymentUpdateService {
 
         try {
             Bill bill = paymentDetail.getBill();
-
-            TaskCriteria criteria = TaskCriteria.builder()
-                    .taskNumber(bill.getConsumerCode())
-                    .build();
-
-            List<Task> tasks = repository.getApplications(criteria ,null);
-
-            if (CollectionUtils.isEmpty(tasks)) {
-                throw new CustomException("INVALID_RECEIPT", "No Tasks found for the consumerCode " + criteria.getTaskNumber());
-            }
-
-            Role role = Role.builder().code("SYSTEM_ADMIN").tenantId(tenantId).build();
-            requestInfo.getUserInfo().getRoles().add(role);
-
-            for (Task task : tasks) {
-                log.info("Updating pending payment status for task: {}", task);
-                if (task.getTaskType().equals(SUMMON)) {
-                    Workflow workflow = new Workflow();
-                    workflow.setAction("MAKE PAYMENT");
-                    task.setWorkflow(workflow);
-                    String status = workflowUtil.updateWorkflowStatus(requestInfo, tenantId, task.getTaskNumber(),
-                            config.getTaskSummonBusinessServiceName(), workflow, config.getTaskSummonBusinessName());
-                    task.setStatus(status);
-
-                    TaskRequest taskRequest = TaskRequest.builder().requestInfo(requestInfo).task(task).build();
-                    if (ISSUESUMMON.equalsIgnoreCase(status))
-                        producer.push(config.getTaskIssueSummonTopic(), taskRequest);
-
-                    producer.push(config.getTaskUpdateTopic(), taskRequest);
+            
+            String taskNumber = bill.getConsumerCode();
+            if(taskNumber.endsWith(config.getSummonsEpostFeesSufix()) || taskNumber.endsWith(config.getSummonsCourtFeesSufix())){
+                BillResponse billResponse = getBill(requestInfo, bill.getTenantId(), taskNumber);
+                Bill bill1 = billResponse.getBill().get(0);
+                if(bill1.getStatus().equals(Bill.StatusEnum.PAID)){
+                    updatePaymentSuccessWorkflow(requestInfo, tenantId, bill1);
+                }
+                else {
+                    return;
                 }
             }
+
+            updatePaymentSuccessWorkflow(requestInfo, tenantId, bill);
         } catch (Exception e) {
             log.error("Error updating workflow for task payment: {}", e.getMessage(), e);
+        }
+    }
+
+    private void updatePaymentSuccessWorkflow(RequestInfo requestInfo, String tenantId, Bill bill) {
+        TaskCriteria criteria = TaskCriteria.builder()
+                .taskNumber(bill.getConsumerCode())
+                .build();
+
+        List<Task> tasks = repository.getApplications(criteria ,null);
+
+        if (CollectionUtils.isEmpty(tasks)) {
+            throw new CustomException("INVALID_RECEIPT", "No Tasks found for the consumerCode " + criteria.getTaskNumber());
+        }
+
+        Role role = Role.builder().code("TASK_UPDATOR").tenantId(tenantId).build();
+        requestInfo.getUserInfo().getRoles().add(role);
+
+        for (Task task : tasks) {
+            log.info("Updating pending payment status for task: {}", task);
+            if (task.getTaskType().equals(SUMMON)) {
+                Workflow workflow = new Workflow();
+                workflow.setAction("MAKE PAYMENT");
+                task.setWorkflow(workflow);
+                String status = workflowUtil.updateWorkflowStatus(requestInfo, tenantId, task.getTaskNumber(),
+                        config.getTaskSummonBusinessServiceName(), workflow, config.getTaskSummonBusinessName());
+                task.setStatus(status);
+
+                TaskRequest taskRequest = TaskRequest.builder().requestInfo(requestInfo).task(task).build();
+                if (ISSUESUMMON.equalsIgnoreCase(status))
+                    producer.push(config.getTaskIssueSummonTopic(), taskRequest);
+
+                producer.push(config.getTaskUpdateTopic(), taskRequest);
+            }
+        }
+    }
+
+    public BillResponse getBill(RequestInfo requestInfo, String tenantId, String taskNumber) {
+        String uri = buildSearchBillURI(tenantId, taskNumber, config.getTaskBusinessService());
+
+        org.egov.common.contract.models.RequestInfoWrapper requestInfoWrapper = RequestInfoWrapper.builder().requestInfo(requestInfo).build();
+
+        Object response = serviceRequestRepository.fetchResult(new StringBuilder(uri), requestInfoWrapper);
+
+        return mapper.convertValue(response, BillResponse.class);
+    }
+
+    private String buildSearchBillURI(String tenantId, String applicationNumber, String businessService) {
+        try {
+            String encodedTenantId = URLEncoder.encode(tenantId, StandardCharsets.UTF_8);
+            String encodedApplicationNumber = URLEncoder.encode(applicationNumber, StandardCharsets.UTF_8);
+            String encodedBusinessService = URLEncoder.encode(businessService, StandardCharsets.UTF_8);
+
+            return URI.create(String.format("%s%s?tenantId=%s&consumerCode=%s&service=%s",
+                    config.getBillingServiceHost(),
+                    config.getSearchBillEndpoint(),
+                    encodedTenantId,
+                    encodedApplicationNumber,
+                    encodedBusinessService)).toString();
+        } catch (Exception e) {
+            log.error("Error occurred when creating bill uri with search params", e);
+            throw new CustomException("GENERATE_BILL_ERROR", "Error Occurred when  generating bill");
         }
     }
 
