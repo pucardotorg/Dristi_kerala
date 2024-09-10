abd9e9488bbdf236505f70b25876b74ac7d800fb
ankitSharmaBeehyv <ankit.sharma@beehyv.com>
2024-09-10 18:44:34 +0530

    fixed dates formatting (#972)

--- backend/transformer/src/main/java/org/egov/transformer/models/Dates.java
+++ backend/transformer/src/main/java/org/egov/transformer/models/Dates.java
@@ -5,18 +5,16 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Dates {
 
-    private LocalDate filingDate;
+    private String filingDate;
 
-    private LocalDate registrationDate;
+    private String registrationDate;
 
-    private LocalDate judgementDate;
+    private String judgementDate;
 
 }
