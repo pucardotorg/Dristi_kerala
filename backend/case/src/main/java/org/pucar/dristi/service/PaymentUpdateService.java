619242a4359e8defc888dd52afaa9a3cd50dea32
Prasannajeet Meshram <107095851+prasanna2400@users.noreply.github.com>
2024-09-10 15:51:54 +0530

    Update PaymentUpdateService.java

--- backend/case/src/main/java/org/pucar/dristi/service/PaymentUpdateService.java
+++ backend/case/src/main/java/org/pucar/dristi/service/PaymentUpdateService.java
@@ -71,17 +71,21 @@ public class PaymentUpdateService {
 
     }
 
-    private void updateWorkflowForCasePayment(RequestInfo requestInfo, String tenantId, PaymentDetail paymentDetail) {
+     private void updateWorkflowForCasePayment(RequestInfo requestInfo, String tenantId, PaymentDetail paymentDetail) {
 
         Bill bill  = paymentDetail.getBill();
 
+        String consumerCode = bill.getConsumerCode();
+        String[] consumerCodeSplitArray = consumerCode.split("_", 2);
+        String fillingNumber=consumerCodeSplitArray[0];
+
         CaseCriteria criteria = CaseCriteria.builder()
-                .filingNumber(bill.getConsumerCode())
+                .filingNumber(fillingNumber)
 //                .tenantId(tenantId)
                 .build();
         List<CaseCriteria> criterias = new ArrayList<>();
         criterias.add(criteria);
-        List<CaseCriteria> caseCriterias = repository.getApplications(criterias, requestInfo);
+        List<CaseCriteria> caseCriterias = repository.getCases(criterias, requestInfo);
 
         if (CollectionUtils.isEmpty(caseCriterias.get(0).getResponseList()))
             throw new CustomException("INVALID RECEIPT",
